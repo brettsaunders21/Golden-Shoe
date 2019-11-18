5ff5b5577ca00adea4dca67ee3c723419c30bfd3
Brett Saunders <bjs730@student.bham.ac.uk>
2019-11-18 23:47:25 +0000

    Cart Remove

--- js/cart.js
+++ js/cart.js
@@ -27,21 +27,65 @@ function addToCart(data) {
     sessionStorage.setItem('total', total);
     console.log(total)
 
-    var cart = JSON.parse(sessionStorage.getItem( 'cart' )).items;
-    var currentAmount = 1;
+    var cartobj = JSON.parse(sessionStorage.getItem( 'cart' ));
+    var cart = cartobj.items
+    var add = true;
     for (var i = 0; i < cart.length; i++) {
         var obj = cart[i];
         if (obj.name == data.name) {
-            currentAmount += parseInt(obj.amount);
+            add = false;
+            var copy = cartobj;
+            copy.items[i].amount += 1;
+            sessionStorage.setItem('cart',JSON.stringify(copy));
         }
     }
 
-    processAddToCart(
-        {
-            name: data.name,
-            price: data.price,
-            colour: data.colour,
-            amount: parseInt(currentAmount)
+    if (add) {
+        processAddToCart(
+            {
+                name: data.name,
+                price: data.price,
+                colour: data.colour,
+                amount: 1,
+                size: 4
+            }
+        )
+    }
+}
+
+function displayCart() {
+    var cartobj = JSON.parse(sessionStorage.getItem( 'cart' ));
+    var items = cartobj.items;
+
+    items.forEach(function(item) {
+        var details = $(' <a href="javascript:void(0);" onclick="selected(\''+item.name+'\');" class="list-group-item list-group-item-action flex-column align-items-start">' +
+            ' <div class="d-flex w-100 justify-content-between">' +
+            '<h5 class="mb-1" style="font-size: xx-large">'+ item.name +'</h5>' +
+            '<small style="font-size: x-large">x'+ item.amount +'</small>' +
+            '</div>' +
+            '<p class="mb-1" style="font-size: large ">Price: £'+ item.price +'.00.</p>' +
+            ' <small style="font-size: medium">Colour: ' + item.colour +'</small>' +
+            '<br/>' +
+            '<small style="font-size: medium">Size: '+ item.size +'</small>' +
+            '</a>');
+        details.appendTo('#checkout')
+    });
+}
+
+function removeFromCart(item) {
+    var cartobj = JSON.parse(sessionStorage.getItem( 'cart' ));
+    var cartcopy = cartobj;
+    for (var i = 0; i < cartcopy.items.length; i++) {
+        var obj = cartcopy.items[i];
+        if (toString(obj.name) == toString(item)) {
+            console.log("dfgdf")
+            if (cartcopy.items[i].amount > 1) {
+                cartcopy.items[i].amount -= 1;
+            } else {
+                cartcopy.items.splice(i,1);
+            }
+        continue;
         }
-    )
+    }
+    sessionStorage.setItem('cart',JSON.stringify(cartcopy));
 }
